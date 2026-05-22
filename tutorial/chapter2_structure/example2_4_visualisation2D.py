ee242f8921ec9ed356825772f1dd4c922893f65a
rsnichols <rnichols2020@my.fit.edu>
2026-05-22 16:13:48 -0400

    Update example2_4_visualisation2D.py
    changed filename to try1

--- tutorial/chapter2_structure/example2_4_visualisation2D.py
+++ tutorial/chapter2_structure/example2_4_visualisation2D.py
@@ -9,7 +9,8 @@ from plantbox.visualisation import figure_style
 sim_time = 14  # [day]  # |\label{l2_1g:defineStart}|
 plant = MappedPlantPython()  # |\label{l2_1g:MappedPlantPython}|
 path = "../../modelparameter/structural/plant/"
-filename = "fspm2023"
+#filename = "fspm2023"
+filename = "try1"
 plant.readParameters(path + filename + ".xml")
 
 soil_domain = pb.SDF_PlantContainer(500, 500, 500, True)  # to avoid root growing aboveground
